651e0b322cb52dd156c3f4465c0f3e19937fead4
Immolazione <federico.deered@gmail.com>
2022-02-24 17:24:57 +0100

    trying to integer $_GET

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 $phrase = "Ieri ho preso il pane, è molto buono il pane. Quando l' ho portato a casa mia moglie ha eslamato:'PANE!', e poi abbiamo mangiato il pane assieme.";
 
-$bad_words = ['pane', 'Pane', 'PANE'];
+$name = 'pane';
+
+$bad_words = $_GET ? $_GET[$name] : '';
 
 $clean_phrase = str_replace($bad_words, '***', $phrase);
 
